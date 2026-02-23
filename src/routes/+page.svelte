8a0063feec2be0a284ea8e60a36f5e348feb7d72
bsnack <swuott2009@gmail.com>
2026-02-23 10:43:24 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+
+  let results = [];
+
+  function handleInput(event) {
+    const query = event.target.value;
+    results = filterData(query);
+  }
+</script>
+
+<main class="container">
+  <form class="row top" onsubmit={greet}>
+    <input
+      type="search"
+      oninput={handleInput}
+      id="search-query"
+      placeholder="Enter a song title / lyric..."
+      bind:value={name}
+    />
+  </form>
+</main>
+
+<style>
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  .container {
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .row {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    display: flex;
+    justify-content: center;
+  }
+
+  .search-bar {
+  }
+
+  input,
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button:hover {
+    border-color: #396cd8;
+  }
+  button:active {
+    border-color: #396cd8;
+    background-color: #e8e8e8;
+  }
+
+  input,
+  button {
+    outline: none;
+  }
+
+  #search-query {
+    margin-right: 5px;
+  }
+
+  .top {
+    height: 60px;
+    display: flex;
+    flex-direction: columns;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #2f2f2f;
+    }
+
+    input,
+    button {
+      color: #ffffff;
+      background-color: #0f0f0f98;
+    }
+    button:active {
+      background-color: #0f0f0f69;
+    }
+  }
+</style>
